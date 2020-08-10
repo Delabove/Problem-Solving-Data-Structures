373b78af895040a72b53f8ae78740399e4db954a
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-10 18:39:07 -0500

    First commit

---
+++ src/com/codewithdelayne/DynamicArray.java
@@ -0,0 +1,11 @@
+package com.codewithdelayne;
+import java.util.List;
+
+public class DynamicArray {
+
+   private List<List<Integer>> seqList;
+
+    public static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
+
+
+    }
---
+++ src/com/codewithdelayne/Main.java
@@ -0,0 +1,13 @@
+package com.codewithdelayne;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<List<Integer>> queries = new ArrayList<>();
+
+
+    }
+}
