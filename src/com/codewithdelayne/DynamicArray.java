92aa518312ad5eb42b954b19cd23d8820807c265
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-11 00:06:59 -0500

    Working on algorithm

--- src/com/codewithdelayne/DynamicArray.java
+++ src/com/codewithdelayne/DynamicArray.java
@@ -1,11 +1,36 @@
 package com.codewithdelayne;
+
+import java.util.ArrayList;
 import java.util.List;
+
+
 
 public class DynamicArray {
 
-   private List<List<Integer>> seqList;
 
-    public static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
+    private static List<Integer> seqList;
 
+    public static List<Integer> dynamicArray(int n, List<List<Integer>> queries){
+        int x = 0;
+        int lastAnswer = 0;
+
+        for (int i = 0; i <= 2; i++)
+            for (int j = 0; j <= 2; j++)
+
+        }
 
     }
+}
+
+}
+
+//   INPUT  L.ANSW
+//    (NQ)     7
+//     25      3
+//
+//   (QXY)
+//   Q1 105
+//   Q2 117
+//   Q1 103
+//   Q2 210
+//   Q1 211
--- src/com/codewithdelayne/Main.java
+++ src/com/codewithdelayne/Main.java
@@ -6,8 +6,23 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<List<Integer>> queries = new ArrayList<>();
+        List<List<Integer>> seqList = new ArrayList<>(2);
+
+        /*Adding 3 to 0th row created above x(0, 0)*/
+        seqList.get(0).add(0, 0);
+        seqList.get(1).add(1, 1);
 
 
     }
 }
+
+//   INPUT  L.ANSW
+//    (NQ)     7
+//     25      3
+//
+//   (QXY)
+//   Q1 105
+//   Q2 117
+//   Q1 103
+//   Q2 210
+//   Q1 211
