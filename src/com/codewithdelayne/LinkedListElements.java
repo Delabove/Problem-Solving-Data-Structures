e7d7a0a7803d46ee6309d1e23cbeefff5bebf4f5
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-11 18:55:37 -0500

    Singly linked list structure

---
+++ src/com/codewithdelayne/LinkedListElements.java
@@ -0,0 +1,19 @@
+package com.codewithdelayne;
+
+import org.w3c.dom.Node;
+
+import java.util.LinkedList;
+
+public class LinkedListElements {
+    Node head;
+
+    class SinglyLinkedListNode{
+        int data;
+        Node next;
+    }
+
+    static  void printLinkedList(SinglyLinkedListNode head) {
+
+
+    }
+}
