9a758fa19d8f15b5b37ed38c31e56a9b8b437527
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-11 08:31:03 -0500

    Dynamic Array Passed

--- src/com/codewithdelayne/DynamicArray.java
+++ src/com/codewithdelayne/DynamicArray.java
@@ -8,51 +8,59 @@ import java.util.List;
 public class DynamicArray {
 
 
-    private static List<List<Integer>> seqList;
-
     public static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
-        List<Integer> result = new ArrayList<>();
+        List<List<Integer>> seqList = new ArrayList<>();
+        List<Integer> seq =new ArrayList<Integer>();
+
         int lastAnswer = 0;
 
         for (int i = 0; i <= n; i++) {
-            seqList.add(new ArrayList<>());
+            seq= new ArrayList<Integer>();
+            seqList.add(new ArrayList<Integer>());
 
         }
 
+        List<Integer> updateAnswer = new ArrayList<>();
 
         for (List<Integer> q : queries) {
+
             int index;
-            int size;
+            int size=0;
             int queryType= q.get(0);
             int x = q.get(1);
             int y = q.get(2);
 
-            if (q.get(0) == 1) {
-               index = (x^lastAnswer)%n;
-               seqList.get(index).add(y);
+            if (queryType == 1) {
+                index = (x^lastAnswer)%n;
+                seqList.get(index).add(y);
 
             }
-            if(queryType==2){
-                index = (x^lastAnswer)
+            if(queryType == 2){
+                index = (x ^ lastAnswer) % n;
+                size=seqList.get(index).size();
+                lastAnswer= seqList.get(index).get(y % size);
+                updateAnswer.add(lastAnswer);
             }
         }
-        return result;
+        return updateAnswer;
     }
+
 }
+
 
 
 //Q1
 //CREATE LIST OF INTEGERS
 //FIND SEQ[0] INDEX
 // CHECK TO SEE IF INDEX(X) || LAST ANSWER
-//  IF INDEX == 0 && LASTANSWER ==0 THEN Y == 0;
+//  IF INDEX == 0 && LAST ANSWER ==0 THEN Y == 0;
 
 //APPEND Y TO END OF THAT ELEMEMNT;
 //    .add to element array
 
 
 //Q2
-//FIND SEQ[1] INDEX[X] OR LASTANSWER
+//FIND SEQ[1] INDEX[X] OR LAST ANSWER
 //FIND VALUE OF ELEMENT Y%SIZE AND ASSIGN IT TO LAST ANSWER;
 //PRINT VALUE OF LAST ANSWER ON A NEW LINE
 
--- src/com/codewithdelayne/Main.java
+++ src/com/codewithdelayne/Main.java
@@ -1,50 +1,10 @@
 package com.codewithdelayne;
-
-import javax.naming.spi.DirStateFactory;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-        int n = Integer.parseInt(firstMultipleInput[0]);
-
-        int q = Integer.parseInt(firstMultipleInput[1]);
-
-        List<List<Integer>> queries = new ArrayList<>();
-
-        for (int i = 0; i < q; i++) {
-            String[] queriesRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-            List<Integer> queriesRowItems = new ArrayList<>();
-
-            for (int j = 0; j < 3; j++) {
-                int queriesItem = Integer.parseInt(queriesRowTempItems[j]);
-                queriesRowItems.add(queriesItem);
-            }
-
-            queries.add(queriesRowItems);
-        }
-
-        List<Integer> result = DirStateFactory.Result.dynamicArray(n, queries);
-
-        for (int i = 0; i < result.size(); i++) {
-            bufferedWriter.write(String.valueOf(result.get(i)));
-
-            if (i != result.size() - 1) {
-                bufferedWriter.write("\n");
-            }
-        }
-
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
     }
 }
 
