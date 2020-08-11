242a787a6403f92d9d9e5576d9440839ef8de752
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-11 11:41:34 -0500

    Separated methods to first initialize index and rotations then another to store the rotated array inside the new array

--- src/com/codewithdelayne/LeftRotation.java
+++ src/com/codewithdelayne/LeftRotation.java
@@ -1,23 +1,39 @@
 package com.codewithdelayne;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class LeftRotation {
     //input:
     // int[]
     // int d= # of rotations
-    public int[] rotateLeft(int[] a, int d){
+    public static void rotateLeft(int[] a, int d) {
 
-    int n = a.length;
-    int[] rotatedArray= new int[n];
-
-    for(int i = 0; i < a.length-1; i++){
-        a[i] = a[n - 1];
+        int[] rotated = rotate(a, d);
+        for (int i = 0; i < rotated.length; i++) {
+            if (i != 0) {
+                System.out.print(" ");
+            }
+            System.out.print(rotated[i]);
+        }
+        System.out.println();
     }
-        return a;
+    static int[] rotate(int[] a, int d) {
+        int[] rotated = new int[a.length];
+        for (int i = 0; i < rotated.length; i++) {
+            rotated[i] = a[(i + d) % a.length];
+        }
+        return rotated;
     }
 
+    public static void main(String[] args) {
+       int[] a = {1, 2, 3, 4, 5};
+
+        System.out.println("Original: " + Arrays.toString(a));
+        System.out.println("Rotated: "); rotateLeft(a, 2);
 
 
-
+    }
 
 
 }
