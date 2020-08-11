b6cb04314820572e98a723bf16a68724f3e0ab2e
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-11 12:17:57 -0500

    Setting up Sparse Array algorithm and function

---
+++ src/com/codewithdelayne/SparseArrays.java
@@ -0,0 +1,59 @@
+package com.codewithdelayne;
+
+public class SparseArrays {
+
+//    / Complete the matchingStrings function below.
+    static int[] matchingStrings(String[] strings, String[] queries) {
+
+        // strings size
+        int n= strings.length;
+        //queries
+        int q= queries.length;
+
+
+
+
+    }
+
+
+
+}
+
+
+// COLLECTION["input", "input", "input"]
+//// COLLECTION["query", "query", "query"]
+// for each "query" DETERMINE # of times it occurs in ["input", "input", "input"]
+//ASKING FOR: NUMBER OF TIMES A QUERY OCCURS IN LIST OF INPUTS
+
+//EXAMPLE:
+
+//for input strings: ['ab', 'ab', 'abc']
+//and query strings: ['ab', 'abc', 'bc']
+// 2 instances: 'ab'
+//1 instance: 'abc'
+//0 instances: 'bc'
+//____________________________
+// store instances in results array: [2,1,0]
+
+//function description:
+//complete the function matchingStrings
+
+
+
+//>>>>> RETURN ARRAY OF INTEGERS REPRESENTING HOW MANY INSTANCES OF EACH QUERY<<<<<<<<<<<<<<
+//DATA NEEDED TO RUN FUNCTION:
+// strings = int n= strings.length;
+//LineN = n[i]
+//queries= int q= queries.length;
+//LineQ = q[i]
+
+//parameters:
+//strings -SEARCH
+// queries
+
+//SAMPLE OUTPUT
+// 2
+// 1
+// 0
+
+// ALGORITHM:
