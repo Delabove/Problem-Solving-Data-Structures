24629bd717c14b6ef78e206bebfa68bf369b7c3c
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-11 14:06:27 -0500

    HashMap to assign the query element with a value then loop through the string array to count its occurrences

--- src/com/codewithdelayne/SparseArrays.java
+++ src/com/codewithdelayne/SparseArrays.java
@@ -1,98 +1,43 @@
 package com.codewithdelayne;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class SparseArrays {
 
-//    / Complete the matchingStrings function below.
-    static int[] matchingStrings(String[] strings, String[] queries) {
 
-        // strings size
-        int n= strings.length;
-        //queries
-        int q= queries.length;
-//        counter for instances
-        int count = 0;
-        int [] results = new int[q];
+        static int[] matchingStrings(String[] strings, String[] queries) {
+            Map<String, Integer> map = new HashMap<>();
+            int result[] = new int[queries.length];
 
-            //dynamic
-            if(results.length == count) {
-                int[] newResults = new int[count + 1];
+            for (int i = 0; i < strings.length; i++) {
+                String inputString = strings[i];
+                if (map.containsKey(inputString)) {
+                    map.put(inputString, map.get(inputString) + 1);
+                } else {
+                    map.put(inputString, 1);
+                }
             }
 
-//          loop through
-            for( int i = 0; i < n; i++){
-               if(strings[i].equals(queries[i])){
-                    count = count + 1;
-                   results[i]= count;
-                   count++;
-               }
-
+            for (int i = 0; i < queries.length; i++) {
+                String queryString = queries[i];
+                if (map.containsKey(queryString)) {
+                    result[i] = map.get(queryString);
+                }
             }
 
-        return results;
+            return result;
+        }
+
+        public static void main(String[] args) {
+
+            String[] strings= {"farewell", "hello", "goodbye", "hola", "farewell", "goodbye", "goodbye"};
+            String[] queries= {"hello", "goodbye", "hola", "farewell"};
+
+            System.out.println(Arrays.toString(matchingStrings(strings, queries)));
+
+        }
+
     }
-
-
-    public static void main(String[] args) {
-        String[] strings = {"hello", "goodbye", "hi", "hola", "hello"};
-        String[] queries = {"hey", "hola", "see ya", "goodbye"};
-        System.out.println("String array: " + Arrays.toString(strings));
-        System.out.println(" Query array: " + Arrays.toString(queries));
-
-        matchingStrings(strings, queries);
-    }
-}
-// COLLECTION["input", "input", "input"]
-//// COLLECTION["query", "query", "query"]
-// for each "query" DETERMINE # of times it occurs in ["input", "input", "input"]
-//ASKING FOR: NUMBER OF TIMES A QUERY OCCURS IN LIST OF INPUTS
-
-//EXAMPLE:
-
-//for input strings: ['ab', 'ab', 'abc']
-//and query strings: ['ab', 'abc', 'bc']
-// 2 instances: 'ab'
-//1 instance: 'abc'
-//0 instances: 'bc'
-//____________________________
-// store instances in results array: [2,1,0]
-
-//function description:
-//complete the function matchingStrings
-
-
-
-//>>>>> RETURN ARRAY OF INTEGERS REPRESENTING HOW MANY INSTANCES OF EACH QUERY<<<<<<<<<<<<<<
-
-// loop through n ['ab', 'ab', 'abc'] to find n[i]== q[i]
-//when we find 1, add to counter, then add count to end of result array
-//restart loop for next count
-//once length is reached return results array
-
-//DATA NEEDED TO RUN FUNCTION:
-// strings = int n= strings.length;
-//LineN = n[i]
-//queries= int q= queries.length;
-//LineQ = q[i]
-
-//parameters:
-//strings -SEARCH
-// queries
-
-//SAMPLE OUTPUT
-// 2
-// 1
-// 0
-
-// ALGORITHM:
-// to count instances:
-//
-// for(q : queries)
-//
-// count = count + 1
-//result= count;
-//reset counter
-
-
-// DETERMINE # of times it occurs in ["input", "input", "input"]
