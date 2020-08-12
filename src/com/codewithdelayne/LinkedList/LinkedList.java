7b52e4aee75c172fbf2e8fae5a3e45af04419225
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-11 22:01:50 -0500

    Added print method to print position of insertion

--- src/com/codewithdelayne/LinkedList/LinkedList.java
+++ src/com/codewithdelayne/LinkedList/LinkedList.java
@@ -5,15 +5,21 @@ public class LinkedList {
 
     static class SinglyLinkedListNode{
         int data;
+        public SinglyLinkedListNode next;
 
-        SinglyLinkedListNode next;
 
 
         public SinglyLinkedListNode(int data) {
+
+            this.data= data;
         }
+
+    }
+    static SinglyLinkedListNode GetNode(int data) {
+        return new SinglyLinkedListNode(data);
     }
 
-    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+    public static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode head, int data) {
         SinglyLinkedListNode first= head;
         SinglyLinkedListNode insertNode = new SinglyLinkedListNode(data);
 
@@ -31,7 +37,7 @@ public class LinkedList {
         return head;
     }
 
-    static SinglyLinkedListNode insertNodeAtHead( SinglyLinkedListNode llist, int data) {
+    static SinglyLinkedListNode insertNodeAtTail( SinglyLinkedListNode llist, int data) {
 
         SinglyLinkedListNode last = llist;
         SinglyLinkedListNode insertNode = new SinglyLinkedListNode(data);
@@ -41,4 +47,49 @@ public class LinkedList {
 
         return llist ;
     }
+//    static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
+//
+//
+//    }
+    static void PrintList(SinglyLinkedListNode node) {
+        while (node != null) {
+            System.out.print(node.data);
+            node = node.next;
+            if (node != null)
+                System.out.print(",");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        SinglyLinkedListNode head = GetNode(3);
+        head.next = GetNode(5);
+        head.next.next = GetNode(8);
+        head.next.next.next = GetNode(10);
+
+        System.out.print("Linked list before insertion: ");
+        PrintList(head);
+
+        int data = 12, pos = 3;
+        head = insertNodeAtHead(head, data);
+        System.out.print("Linked list after" + " insertion of 12 at position 3: ");
+        PrintList(head);
+
+        // front of the linked list
+        data = 1;
+        pos = 1;
+        head = insertNodeAtHead(head, data);
+        System.out.print("Linked list after" + "insertion of 1 at position 1: ");
+        PrintList(head);
+
+        // insetion at end of the linked list
+        data = 15;
+        pos = 7;
+        head = insertNodeAtTail(head, data);
+        System.out.print("Linked list after" + " insertion of 15 at position 7: ");
+        PrintList(head);
+    }
 }
+
+
+
+
