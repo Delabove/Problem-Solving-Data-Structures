e789ef29fd8c4a6e50e500355555c93a2c88a251
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-11 22:54:54 -0500

    Push for the night

--- src/com/codewithdelayne/LinkedList/LinkedList.java
+++ src/com/codewithdelayne/LinkedList/LinkedList.java
@@ -5,13 +5,15 @@ public class LinkedList {
 
     static class SinglyLinkedListNode{
         int data;
+        int position;
         public SinglyLinkedListNode next;
 
 
 
-        public SinglyLinkedListNode(int data) {
+        public SinglyLinkedListNode(int data, int position) {
 
             this.data= data;
+            this.position= position;
         }
 
     }
@@ -47,10 +49,44 @@ public class LinkedList {
 
         return llist ;
     }
-//    static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
-//
-//
-//    }
+    static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
+       SinglyLinkedListNode headnode = head;
+       headnode.data = data;
+       headnode.next = null;
+
+        if (position < 1)
+            System.out.print("Invalid position");
+
+        // if position is 1 then new node is
+        // set infornt of head node
+        // head node is changing.
+        if (position == 1) {
+           SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+            newNode.next = head;
+            head = newNode;
+        } else {
+            while (position-- != 0) {
+                if (position == 1) {
+                    // adding Node at required position
+                    SinglyLinkedListNode newNode = GetNode(data);
+
+                    // Making the new Node to point to
+                    // the old Node at the same position
+                    newNode.next = head.next;
+
+                    // Replacing current with new Node
+                    // to the old Node to point to the new Node
+                    head.next = newNode;
+                    break;
+                }
+                head = head.next;
+            }
+            if (position != 1)
+                System.out.print("Position out of range");
+        }
+        return head;
+
+    }
     static void PrintList(SinglyLinkedListNode node) {
         while (node != null) {
             System.out.print(node.data);
@@ -61,32 +97,33 @@ public class LinkedList {
         System.out.println();
     }
     public static void main(String[] args) {
-        SinglyLinkedListNode head = GetNode(3);
-        head.next = GetNode(5);
-        head.next.next = GetNode(8);
-        head.next.next.next = GetNode(10);
+        SinglyLinkedListNode head = GetNode(16);
+        head.next = GetNode(3);
+        head.next.next = GetNode(7);
+        head.next.next.next = GetNode(1);
+
 
         System.out.print("Linked list before insertion: ");
         PrintList(head);
 
-        int data = 12, pos = 3;
-        head = insertNodeAtHead(head, data);
-        System.out.print("Linked list after" + " insertion of 12 at position 3: ");
+        int data = 1; position = 2;
+        head = insertNodeAtPosition(head, data, position);
+        System.out.print("Linked list after" + " insertion of 1 at position 2: ");
         PrintList(head);
 
-        // front of the linked list
+        // middle of the linked list
         data = 1;
-        pos = 1;
-        head = insertNodeAtHead(head, data);
+        position = 4;
+        head = insertNodeAtPosition(head, data, position);
         System.out.print("Linked list after" + "insertion of 1 at position 1: ");
         PrintList(head);
 
-        // insetion at end of the linked list
-        data = 15;
-        pos = 7;
-        head = insertNodeAtTail(head, data);
-        System.out.print("Linked list after" + " insertion of 15 at position 7: ");
-        PrintList(head);
+        // insertion at end of the linked list
+      // list data = 15;
+//        pos = 7;
+//        head = insertNodeAtTail(head, data);
+//        System.out.print("Linked list after" + " insertion of 15 at position 7: ");
+//        PrintList(head);
     }
 }
 
