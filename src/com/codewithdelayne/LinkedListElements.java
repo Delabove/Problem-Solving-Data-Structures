e1703edb7064db26c229d0fdf3b314b51a573cef
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-11 19:23:25 -0500

    Declared node fields and creating linked list

--- src/com/codewithdelayne/LinkedListElements.java
+++ src/com/codewithdelayne/LinkedListElements.java
@@ -2,18 +2,40 @@ package com.codewithdelayne;
 
 import org.w3c.dom.Node;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class LinkedListElements {
     Node head;
 
-    class SinglyLinkedListNode{
-        int data;
-        Node next;
+
+    private class SinglyLinkedListNode{
+       private int data;
+       private Node next;
+
+        void Node(int d) { data = d; }
+
+
     }
+
+    private Node first;
+    private Node last;
 
     static  void printLinkedList(SinglyLinkedListNode head) {
+        var node = new Node();
 
 
     }
+
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add(0, 16);
+        list.add(1, 13);
+
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
+    }
+
 }
