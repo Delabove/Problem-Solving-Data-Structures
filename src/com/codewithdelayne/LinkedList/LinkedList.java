6513bac81ecf29be7b7ba2e9a6e8dfb6ec51bf13
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-12 15:33:35 -0500

    Push most recent code

--- src/com/codewithdelayne/LinkedList/LinkedList.java
+++ src/com/codewithdelayne/LinkedList/LinkedList.java
@@ -101,21 +101,6 @@ public class LinkedList {
 
     }
 
-    static void reversePrint(Node head) {
-        Node prev = head;
-        Node current = head.next;
-
-        while(current != null){
-             Node next = current.next;
-             current.next = prev;
-
-             prev = current;
-             current = next;
-
-        }
-
-    }
-
 
     //helper function to print given list
     static void PrintList(Node head) {
@@ -143,8 +128,8 @@ public class LinkedList {
         insertNodeAtPosition(6,5);
 
         deleteNode( 0);
-
-        reversePrint(head);
+        deleteNode( 1);
+        deleteNode( 2);
         PrintList(head);
     }
 }
