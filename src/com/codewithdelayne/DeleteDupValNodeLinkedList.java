38f5a5826e722b72df5e99da74d2dc7e2a2c5591
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-19 20:57:27 -0500

    Delete Duplicate Value Nodes in a Linked List

---
+++ src/com/codewithdelayne/DeleteDupValNodeLinkedList.java
@@ -0,0 +1,67 @@
+package com.codewithdelayne;
+
+public class DeleteDupValNodeLinkedList {
+
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+
+
+    static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode head) {
+        SinglyLinkedListNode node = head;
+        if (head == null) {
+            return null;
+        }
+        while (head.next != null) {
+            if (head.data == head.next.data) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+        return node;
+    }
+
+}
+
+//
+// Delete as few nodes as possible so that the list does not contain any value more than once
+//
+// head pointer may be null
+//
+//
+//
+//
+//
+//
+//
