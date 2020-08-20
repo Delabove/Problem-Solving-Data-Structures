c1e629a960c7ef1655ce22eaa9443da69f000d34
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-19 21:05:02 -0500

    Cycle Detection

---
+++ src/com/codewithdelayne/CycleDetection.java
@@ -0,0 +1,51 @@
+package com.codewithdelayne;
+
+public class CycleDetection {
+
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static boolean hasCycle(SinglyLinkedListNode head) {
+        if(head == null) return false;
+        SinglyLinkedListNode slow = head, fast = head;
+        while(slow != null && fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
